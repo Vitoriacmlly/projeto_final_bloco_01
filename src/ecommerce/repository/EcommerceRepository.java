0c2dcaa326a0c28f0de0ddcae9c04084a4df8576
Vitoriacmlly <vcamilly18@gmail.com>
2025-04-04 11:54:54 -0300

    implements model-repository

--- src/ecommerce/repository/EcommerceRepository.java
+++ src/ecommerce/repository/EcommerceRepository.java
@@ -1,5 +1,15 @@
 package ecommerce.repository;
 
+import ecommerce.model.Produto;
+
 public interface EcommerceRepository {
+	
+	public void procurarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar();
+	public void atualizar();
+	public void deletar(int numero);
+	
+	
 
 }
